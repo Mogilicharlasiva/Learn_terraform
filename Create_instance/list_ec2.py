33df6c658f6751d95dcd02b3b07c458d452b28d3
Mogilicharlasiva <mogilicharlasiva@gmail.com>
2024-08-06 18:12:01 -0400

    notify the status of instances

--- Create_instance/list_ec2.py
+++ Create_instance/list_ec2.py
@@ -1,7 +1,18 @@
-import boto3
 import json
+import boto3
 
-client=boto3.client('ec2')
-
-response=client.describe_instances()
-print(response)
+def lambda_handler(event, context):
+    # TODO implement
+    client=boto3.client('ec2')
+    response=client.describe_instances()
+    for data in response['Reservations']:
+        for ec2_val in data['Instances']:
+            instance_id=(ec2_val['InstanceId'])
+            status=(ec2_val['State']['Name'])
+        if status=="running":
+        # TODO: write code...
+            raise Exception(f"Instance {instance_id} is running.")
+    return {
+        'statusCode': 200,
+        'body': json.dumps("success")
+        }
